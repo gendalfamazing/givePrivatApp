7d50134a57bb03b16d66ba175586a985550b0133
art1fackt <artifackt11@gmail.com>
2024-03-30 15:12:49 +0300

    add manipulation
    add nebulaiser & zond

---
+++ 8button/Views/StudMaterials/Manipulatii/08Nebulaiser.swift
@@ -0,0 +1,18 @@
+//
+//  08Nebulaiser.swift
+//  8button
+//
+//  Created by Artur Vladymcev on 30.03.24.
+//
+
+import SwiftUI
+
+struct _8Nebulaiser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _8Nebulaiser()
+}
---
+++ 8button/Views/StudMaterials/Manipulatii/09Zond.swift
@@ -0,0 +1,18 @@
+//
+//  09Zond.swift
+//  8button
+//
+//  Created by Artur Vladymcev on 30.03.24.
+//
+
+import SwiftUI
+
+struct _9Zond: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _9Zond()
+}
