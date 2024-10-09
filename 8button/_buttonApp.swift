389e159476caaaac560eb50ed43581979c2bed5b
art1fackt <artifackt11@gmail.com>
2024-10-09 21:51:46 +0300

    изменения баттон ап
    чтобы корректно отображались размеры шрифтов

--- 8button/_buttonApp.swift
+++ 8button/_buttonApp.swift
@@ -16,7 +16,8 @@ struct _buttonApp: App {
 
     @StateObject
     private var purchaseManager: PurchaseManager
-
+    @AppStorage("selectedTheme") var selectedTheme: String = Theme.system.rawValue
+    @AppStorage("fontSize") var fontSize: Double = 14.0
     init() {
         let entitlementManager = EntitlementManager()
         let purchaseManager = PurchaseManager(entitlementManager: entitlementManager)
@@ -27,13 +28,34 @@ struct _buttonApp: App {
     var body: some Scene {
         WindowGroup {
             AppTabBarView()
-        
+                .preferredColorScheme(colorScheme)
+                .environment(\.sizeCategory, fontSizeCategory)
                 .environmentObject(entitlementManager)
                 .environmentObject(purchaseManager)
                 .task {
                     await purchaseManager.updatePurchasedProducts()
                 }
         }
+        var colorScheme: ColorScheme? {
+            switch Theme(rawValue: selectedTheme) ?? .system {
+            case .light:
+                return .light
+            case .dark:
+                return .dark
+            case .system:
+                return nil
+            }
+        }
+        var fontSizeCategory: ContentSizeCategory {
+                switch fontSize {
+                case ..<14: return .small
+                case 14..<16: return .medium
+                case 16..<18: return .large
+                case 18..<20: return .extraLarge
+                case 20..<22: return .extraExtraLarge
+                default: return .extraExtraExtraLarge
+                }
+            }
     }
 }
 
