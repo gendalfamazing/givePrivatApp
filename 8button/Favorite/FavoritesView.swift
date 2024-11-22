879c59fd8bdd795e00d3053db03a783e05e6fb88
art1fackt <artifackt11@gmail.com>
2024-11-22 15:50:02 +0300

    добавлено 40 алгоритмов 1030 в избранное

--- 8button/Favorite/FavoritesView.swift
+++ 8button/Favorite/FavoritesView.swift
@@ -1,116 +1,7 @@
 // FavoritesView.swift
 
 import SwiftUI
-struct ViewFactory {
-    static func view(for identifier: String) -> AnyView {
-        switch identifier {
-        case "GENEVA":
-            return AnyView(Geneva())
-                
-        case "Приказы и постановления":
-            return AnyView(PrikazyPostanovleniyaFavorites())
-        case "МКБ-10":
-            return AnyView(CodesMkb10Favorites())
-        case "Шкалы и таблицы":
-            return AnyView(ScalesTablesFavorites())
-        case "Учебные материалы":
-            return AnyView(StudMaterialsFavorites())
-        case "Калькулятор Допамина":
-            return AnyView(InfusionRateCalculatorViewFavorites())
-        case "Акушерское пособие":
-            return AnyView(AkusherskoePosobieFavorites())
-        case "Атлас ЭКГ":
-            return AnyView(AtlasECGFavorites())
-        case "Поиск детских дозировок":
-            return AnyView(FastChildDosesFavorites())
-        case "Постановление №118 (детские протоколы)":
-            return AnyView(Postanovlenie118ViewFavorites())
-        case "Алгоритм 1. «Порядок оказания скорой (неотложной) медицинской помощи»":
-            return AnyView(Prikaz1030Alg1ViewFavorites())
-        case "Алгоритм 2. «Первичный осмотр пациента (ABCD)»":
-            return AnyView(Prikaz1030Alg2ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 4. «Внезапная смерть, сердечно-легочная реанимация»":
-            return AnyView(Prikaz1030Alg4ViewFavorites())
-        case "Алгоритм 5. «Гиповолемический шок»":
-            return AnyView(Prikaz1030Alg5ViewFavorites())
-        case "Алгоритм 6. «Фибрилляция желудочков (ФЖ), желудочковая тахикардия (ЖТ) с острой сердечной недостаточностью»":
-            return AnyView(Prikaz1030Alg6ViewFavorites())
-        case "Алгоритм 7. «Асистолия»":
-            return AnyView(Prikaz1030Alg7ViewFavorites())
-        case "Алгоритм 8. «Электромеханическая диссоциация (нарушение насосной функции при сохранении электрической активности)»":
-            return AnyView(Prikaz1030Alg8ViewFavorites())
-        case "Алгоритм 9. «Постреанимационная поддержка»":
-            return AnyView(Prikaz1030Alg9ViewFavorites())
-        case "Алгоритм 10. «Пароксизмальная тахикардия с узким комплексом QRS»":
-            return AnyView(Prikaz1030Alg10ViewFavorites())
-        case "Алгоритм 11. «Пароксизмальная тахикардия с широким комплексом QRS»":
-            return AnyView(Prikaz1030Alg11ViewFavorites())
-        case "Алгоритм 12. «Желудочковая экстрасистолия (злокачественная)»":
-            return AnyView(Prikaz1030Alg12ViewFavorites())
-        case "Алгоритм 13. «Брадиаритмии (синусовая брадикардия, AV-блокада II степени, полная AV-блокада, синдром слабости синусового узла)»":
-            return AnyView(Prikaz1030Alg13ViewFavorites())
-        case "Алгоритм 14. «Пароксизмальная мерцательная аритмия»":
-            return AnyView(Prikaz1030Alg14ViewFavorites())
-        case "Алгоритм 15. «Острый коронарный синдром»":
-            return AnyView(Prikaz1030Alg15ViewFavorites())
-        case "Алгоритм 16. «Кардиогенный шок»":
-            return AnyView(Prikaz1030Alg16ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        case "Алгоритм 3. «Острая дыхательная недостаточность»":
-            return AnyView(Prikaz1030Alg3ViewFavorites())
-        // Добавьте остальные представления
-        default:
-            return AnyView(Text("Неизвестное представление"))
-        }
-    }
-}
+
 
 
 struct FavoritesView: View {
@@ -292,3 +183,113 @@ class SharedState: ObservableObject {
     @Published var isTextExpanded3 = false
 }
 
+struct ViewFactory {
+    static func view(for identifier: String) -> AnyView {
+        switch identifier {
+        case "GENEVA":
+            return AnyView(Geneva())
+                
+        case "Приказы и постановления":
+            return AnyView(PrikazyPostanovleniyaFavorites())
+        case "МКБ-10":
+            return AnyView(CodesMkb10Favorites())
+        case "Шкалы и таблицы":
+            return AnyView(ScalesTablesFavorites())
+        case "Учебные материалы":
+            return AnyView(StudMaterialsFavorites())
+        case "Калькулятор Допамина":
+            return AnyView(InfusionRateCalculatorViewFavorites())
+        case "Акушерское пособие":
+            return AnyView(AkusherskoePosobieFavorites())
+        case "Атлас ЭКГ":
+            return AnyView(AtlasECGFavorites())
+        case "Поиск детских дозировок":
+            return AnyView(FastChildDosesFavorites())
+        case "Постановление №118 (детские протоколы)":
+            return AnyView(Postanovlenie118ViewFavorites())
+        case "Алгоритм 1. «Порядок оказания скорой (неотложной) медицинской помощи»":
+            return AnyView(Prikaz1030Alg1ViewFavorites())
+        case "Алгоритм 2. «Первичный осмотр пациента (ABCD)»":
+            return AnyView(Prikaz1030Alg2ViewFavorites())
+        case "Алгоритм 3. «Острая дыхательная недостаточность»":
+            return AnyView(Prikaz1030Alg3ViewFavorites())
+        case "Алгоритм 4. «Внезапная смерть, сердечно-легочная реанимация»":
+            return AnyView(Prikaz1030Alg4ViewFavorites())
+        case "Алгоритм 5. «Гиповолемический шок»":
+            return AnyView(Prikaz1030Alg5ViewFavorites())
+        case "Алгоритм 6. «Фибрилляция желудочков (ФЖ), желудочковая тахикардия (ЖТ) с острой сердечной недостаточностью»":
+            return AnyView(Prikaz1030Alg6ViewFavorites())
+        case "Алгоритм 7. «Асистолия»":
+            return AnyView(Prikaz1030Alg7ViewFavorites())
+        case "Алгоритм 8. «Электромеханическая диссоциация (нарушение насосной функции при сохранении электрической активности)»":
+            return AnyView(Prikaz1030Alg8ViewFavorites())
+        case "Алгоритм 9. «Постреанимационная поддержка»":
+            return AnyView(Prikaz1030Alg9ViewFavorites())
+        case "Алгоритм 10. «Пароксизмальная тахикардия с узким комплексом QRS»":
+            return AnyView(Prikaz1030Alg10ViewFavorites())
+        case "Алгоритм 11. «Пароксизмальная тахикардия с широким комплексом QRS»":
+            return AnyView(Prikaz1030Alg11ViewFavorites())
+        case "Алгоритм 12. «Желудочковая экстрасистолия (злокачественная)»":
+            return AnyView(Prikaz1030Alg12ViewFavorites())
+        case "Алгоритм 13. «Брадиаритмии (синусовая брадикардия, AV-блокада II степени, полная AV-блокада, синдром слабости синусового узла)»":
+            return AnyView(Prikaz1030Alg13ViewFavorites())
+        case "Алгоритм 14. «Пароксизмальная мерцательная аритмия»":
+            return AnyView(Prikaz1030Alg14ViewFavorites())
+        case "Алгоритм 15. «Острый коронарный синдром»":
+            return AnyView(Prikaz1030Alg15ViewFavorites())
+        case "Алгоритм 16. «Кардиогенный шок»":
+            return AnyView(Prikaz1030Alg16ViewFavorites())
+        case "Алгоритм 17. «Отек легких»":
+            return AnyView(Prikaz1030Alg17ViewFavorites())
+        case "Алгоритм 18. «Тромбоэмболия легочной артерии»":
+            return AnyView(Prikaz1030Alg18ViewFavorites())
+        case "Алгоритм 19. «Расслаивающая аневризма аорты»":
+            return AnyView(Prikaz1030Alg19ViewFavorites())
+        case "Алгоритм 20. «Острый тромбоз артерий и глубоких вен»":
+            return AnyView(Prikaz1030Alg20ViewFavorites())
+        case "Алгоритм 21. «Гипертонический криз»":
+            return AnyView(Prikaz1030Alg21ViewFavorites())
+        case "Алгоритм 22. «Обморок»":
+            return AnyView(Prikaz1030Alg22ViewFavorites())
+        case "Алгоритм 23. «Приступ бронхиальной астмы»":
+            return AnyView(Prikaz1030Alg23ViewFavorites())
+        case "Алгоритм 24. «Пневмония»":
+            return AnyView(Prikaz1030Alg24ViewFavorites())
+        case "Алгоритм 25. «Стеноз гортани»":
+            return AnyView(Prikaz1030Alg25ViewFavorites())
+        case "Алгоритм 26. «Обструкция дыхательных путей инородным телом»":
+            return AnyView(Prikaz1030Alg26ViewFavorites())
+        case "Алгоритм 27. «Кома неясного генеза»":
+            return AnyView(Prikaz1030Alg27ViewFavorites())
+        case "Алгоритм 28. «Комы при сахарном диабете»":
+            return AnyView(Prikaz1030Alg28ViewFavorites())
+        case "Алгоритм 29. «Судорожный синдром»":
+            return AnyView(Prikaz1030Alg29ViewFavorites())
+        case "Алгоритм 30. «Острое нарушение мозгового кровообращения»":
+            return AnyView(Prikaz1030Alg30ViewFavorites())
+        case "Алгоритм 31. «Гипертермия»":
+            return AnyView(Prikaz1030Alg31ViewFavorites())
+        case "Алгоритм 32. «Высокопатогенный грипп»":
+            return AnyView(Prikaz1030Alg32ViewFavorites())
+        case "Алгоритм 33. «Менингиальная инфекция»":
+            return AnyView(Prikaz1030Alg33ViewFavorites())
+        case "Алгоритм 34. «Острые кишечные инфекции»":
+            return AnyView(Prikaz1030Alg34ViewFavorites())
+        case "Алгоритм 35. «Острый инфекционный гепатит»":
+            return AnyView(Prikaz1030Alg35ViewFavorites())
+        case "Алгоритм 36. «Почечная колика»":
+            return AnyView(Prikaz1030Alg36ViewFavorites())
+        case "Алгоритм 37. «Носовое кровотечение»":
+            return AnyView(Prikaz1030Alg37ViewFavorites())
+        case "Алгоритм 38. «Острая хирургическая патология органов брюшной полости»":
+            return AnyView(Prikaz1030Alg38ViewFavorites())
+        case "Алгоритм 39. «Черепно-мозговая травма»":
+            return AnyView(Prikaz1030Alg39ViewFavorites())
+        case "Алгоритм 40. «Травма позвоночника»":
+            return AnyView(Prikaz1030Alg40ViewFavorites())
+        // Добавьте остальные представления
+        default:
+            return AnyView(Text("Неизвестное представление"))
+        }
+    }
+}
--- 8button/Views/Prik&Post/Prikaz1030/Prikaz1030Alg108View.swift
+++ 8button/Views/Prik&Post/Prikaz1030/Prikaz1030Alg108View.swift
@@ -160,3 +160,145 @@ struct Prikaz1030Alg8View: View {
 #Preview {
     Prikaz1030Alg8View()
 }
+
+struct Prikaz1030Alg8ViewFavorites: View {
+    @AppStorage("fontSize") var fontSize: Double = 14.0
+    @EnvironmentObject var themeManager: ThemeManager
+    @Environment(\.sizeCategory) var sizeCategory
+    
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    
+    @EnvironmentObject var favoritesManager: FavoritesManager
+    @Environment(\.viewContext) var context: ViewContext
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+    
+    var shouldShowOverlay: Bool {
+            switch context {
+            case .favorites:
+                // В избранном не показываем оверлей
+                return false
+            case .nonFavorites, .other:
+                // В других контекстах показываем оверлей
+                return true
+            }
+        }
+    var isInFavorites: Bool {
+        return favoritesManager.favorites.contains { $0.viewIdentifier == "Алгоритм 8. «Электромеханическая диссоциация (нарушение насосной функции при сохранении электрической активности)»" }
+    }
+    
+    func addToFavorites() {
+        let newItem = FavoriteItem(name: "Приказ №1030", viewIdentifier: "Алгоритм 8. «Электромеханическая диссоциация (нарушение насосной функции при сохранении электрической активности)»", isExpanded: false, isNavigationLink: true)
+        let success = favoritesManager.addItem(newItem)
+        if success {
+            // Элемент успешно добавлен
+        } else {
+            // Элемент уже существует
+            alertMessage = "Этот элемент уже добавлен в избранное"
+            showAlert = true
+        }
+    }
+    
+    func removeFromFavorites() {
+        if let item = favoritesManager.favorites.first(where: { $0.viewIdentifier == "Алгоритм 8. «Электромеханическая диссоциация (нарушение насосной функции при сохранении электрической активности)»" }) {
+            favoritesManager.removeItem(item)
+        }
+    }
+    
+
+    var body: some View {
+        VStack {
+            NavigationLink(destination: Prikaz1030Alg8View()) {
+                HStack {
+                    Image(systemName: "doc.text.magnifyingglass")
+                        .resizable()
+                        .frame(width: 18, height: 24)
+                    //                                .padding(.horizontal)
+                        .font(.caption2)
+                        .padding(.horizontal, 11.0)
+                        .padding(.vertical, 8.0)
+                        .background(Color.titleNumber)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.shadowGrayRectangle.opacity(0.35), lineWidth: 0.5) // Устанавливаем цвет и ширину границы
+                        )
+                        .foregroundColor(Color.titleNumberForeground)
+                    Spacer()
+                    Spacer()
+                    Text("Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030Приказ 1030")
+                        .padding(.horizontal, 3.0)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Spacer()
+                    Image(systemName: ("chevron.right"))
+                        .opacity(0.3)
+                        .padding(.trailing, 15)
+                        .padding(.leading, 10)
+                }
+                .padding(5.0)
+                //        .lineLimit(2)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(minHeight: 50)
+                .modifier(ThemeTitleBlueColorModifier())
+                .cornerRadius(10)
+                .overlay(
+                    HStack {
+                        Spacer()
+                        VStack {
+                            if shouldShowOverlay {
+                                Image(systemName: "star.fill")
+                                    .resizable()
+                                    .frame(width: 14, height: 14)
+                                    .foregroundColor(isInFavorites ? .favoriteStar : Color.clear)
+                                    .rotationEffect(.degrees(isInFavorites ? -360 : 0))
+                                    .animation(.snappy, value: isInFavorites)
+                                    .padding(.vertical, 3)
+                                    .padding(.horizontal, 3)
+                            }
+                            Spacer()
+                        }
+                    }
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.shadowGrayRectangle.opacity(0.35), lineWidth: 0.5) // Устанавливаем цвет и ширину границы
+                )
+            }
+        }
+        .padding(2)
+        .contextMenu {
+            switch context {
+            case .favorites:
+                EmptyView()
+            case .nonFavorites:
+                if isInFavorites {
+                    Button(action: {
+                        removeFromFavorites()
+                    }) {
+                        Text("Удалить из избранного")
+                        Image(systemName: "star.slash")
+                    }
+                } else {
+                    Button(action: {
+                        addToFavorites()
+                    }) {
+                        Text("Добавить в избранное")
+                        Image(systemName: "star.fill")
+                    }
+                }
+            default:
+                EmptyView()
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(alertMessage))
+        }
+        
+    }
+    
+}
