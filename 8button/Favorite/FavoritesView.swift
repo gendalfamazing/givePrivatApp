970cd0c46da6deae51f2a3b5297d4e0cf82a5205
art1fackt <artifackt11@gmail.com>
2024-11-24 15:52:06 +0300

    весь 1030 добавлен в избранное

--- 8button/Favorite/FavoritesView.swift
+++ 8button/Favorite/FavoritesView.swift
@@ -287,6 +287,82 @@ struct ViewFactory {
             return AnyView(Prikaz1030Alg39ViewFavorites())
         case "Алгоритм 40. «Травма позвоночника»":
             return AnyView(Prikaz1030Alg40ViewFavorites())
+        case "Алгоритм 41. «Травмы конечностей»":
+            return AnyView(Prikaz1030Alg41ViewFavorites())
+        case "Алгоритм 42. «Травмы груди»":
+            return AnyView(Prikaz1030Alg42ViewFavorites())
+        case "Алгоритм 43. «Травмы живота»":
+            return AnyView(Prikaz1030Alg43ViewFavorites())
+        case "Алгоритм 44. «Политравма»":
+            return AnyView(Prikaz1030Alg44ViewFavorites())
+        case "Алгоритм 45. «Ожоги»":
+            return AnyView(Prikaz1030Alg45ViewFavorites())
+        case "Алгоритм 46. «Тепловой удар»":
+            return AnyView(Prikaz1030Alg46ViewFavorites())
+        case "Алгоритм 47. «Гипотермия»":
+            return AnyView(Prikaz1030Alg47ViewFavorites())
+        case "Алгоритм 48. «Утопление»":
+            return AnyView(Prikaz1030Alg48ViewFavorites())
+        case "Алгоритм 49. «Отравление неизвестным ядом»":
+            return AnyView(Prikaz1030Alg49ViewFavorites())
+        case "Алгоритм 50. «Аллергическая реакция»":
+            return AnyView(Prikaz1030Alg50ViewFavorites())
+        case "Алгоритм 51. «Неотложные состояния в акушерстве и гинекологии»":
+            return AnyView(Prikaz1030Alg51ViewFavorites())
+        case "Алгоритм 52. «Острый реактивный психоз»":
+            return AnyView(Prikaz1030Alg52ViewFavorites())
+        case "Алгоритм 53. «Действия бригады СНМП при ДТП»":
+            return AnyView(Prikaz1030Alg53ViewFavorites())
+        case "Алгоритм 54. «Острое психотическое возбуждение»":
+            return AnyView(Prikaz1030Alg54ViewFavorites())
+        case "Алгоритм 55. «Суицидальное поведение»":
+            return AnyView(Prikaz1030Alg55ViewFavorites())
+        case "Алгоритм 56. «Вертеброгенный болевой синдром (боль в позвоночнике, иррадиация, тоническое напряжение мышц)»":
+            return AnyView(Prikaz1030Alg56ViewFavorites())
+        case "Алгоритм 57. «Мигрень»":
+            return AnyView(Prikaz1030Alg57ViewFavorites())
+        case "Алгоритм 58. «Длительное сдавление мягких тканей»":
+            return AnyView(Prikaz1030Alg58ViewFavorites())
+        case "Алгоритм 59. «Отморожения»":
+            return AnyView(Prikaz1030Alg59ViewFavorites())
+        case "Алгоритм 60. «Поражение электрическим током»":
+            return AnyView(Prikaz1030Alg60ViewFavorites())
+        case "Алгоритм 61. «Странгуляционная асфиксия»":
+            return AnyView(Prikaz1030Alg61ViewFavorites())
+        case "Алгоритм 62. «Острые психотические расстройства при употреблении психоактивных веществ (ПАВ)»":
+            return AnyView(Prikaz1030Alg62ViewFavorites())
+        case "Алгоритм 63. «Побочные эффекты и осложнения психофармакотерапии»":
+            return AnyView(Prikaz1030Alg63ViewFavorites())
+        case "Алгоритм 64. «Острые желудочно-кишечные кровотечения»":
+            return AnyView(Prikaz1030Alg64ViewFavorites())
+        case "Алгоритм 65. «Кровотечение в послеродовом периоде»":
+            return AnyView(Prikaz1030Alg65ViewFavorites())
+        case "Алгоритм 66. «Травма половых органов (женщины)»":
+            return AnyView(Prikaz1030Alg66ViewFavorites())
+        case "Алгоритм 67. «Роды»":
+            return AnyView(Prikaz1030Alg67ViewFavorites())
+        case "Алгоритм 68. «Заглоточный абсцесс»":
+            return AnyView(Prikaz1030Alg68ViewFavorites())
+        case "Алгоритм 69. «Кровотечение из глотки»":
+            return AnyView(Prikaz1030Alg69ViewFavorites())
+        case "Алгоритм 70. «Перелом костей носа и околоносовых пазух»":
+            return AnyView(Prikaz1030Alg70ViewFavorites())
+        case "Алгоритм 71. «Кровотечение из уха»":
+            return AnyView(Prikaz1030Alg71ViewFavorites())
+        case "Алгоритм 72. «Ожоги и травмы глаза, века, конъюнктивы»":
+            return AnyView(Prikaz1030Alg72ViewFavorites())
+        case "Алгоритм 73. «Заболевания глаза, века»":
+            return AnyView(Prikaz1030Alg73ViewFavorites())
+        case "Алгоритм 74. «Инородное тело верхних дыхательных путей, уха»":
+            return AnyView(Prikaz1030Alg74ViewFavorites())
+        case "Алгоритм 75. «Алкогольный абстинентный синдром»":
+            return AnyView(Prikaz1030Alg75ViewFavorites())
+        case "Алгоритм 76. «Острая задержка мочи»":
+            return AnyView(Prikaz1030Alg76ViewFavorites())
+        case "Алгоритм 77. «Респираторная поддержка»":
+            return AnyView(Prikaz1030Alg77ViewFavorites())
+        case "Алгоритм 78. «Алкогольная интоксикация»":
+            return AnyView(Prikaz1030Alg78ViewFavorites())
         // Добавьте остальные представления
         default:
             return AnyView(Text("Неизвестное представление"))
